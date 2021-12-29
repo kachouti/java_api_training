b1e2b1cf99ff024a36db034b833e6da83da7b2e8
Kachouti Mohamed <kachouti36@gmail.com>
2021-12-29 09:53:51 +0100

    add coverage test for postHandler

---
+++ src/test/java/fr/lernejo/navy_battle/PostHandlerTest.java
@@ -0,0 +1,27 @@
+package fr.lernejo.navy_battle;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.net.URI;
+import java.net.http.HttpResponse;
+
+class PostHandlerTest {
+    @Test
+    void postReqTest() throws IOException, InterruptedException {
+
+        new Serveur(1234);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:1234/api/game/start"))
+            .POST(HttpRequest.BodyPublishers.ofString("{\"id\":\"1\", \"url\":\"http://localhost:" + 1234 + "\", \"message\":\"HELLO THE WINNER!\"}")).build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        Assertions.assertEquals(202, response.statusCode());
+    }
+}
