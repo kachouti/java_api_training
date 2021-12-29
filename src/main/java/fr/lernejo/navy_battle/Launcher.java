9143e762a8b3707831a3d8f5b29d658ca2499748
Kachouti Mohamed <kachouti36@gmail.com>
2021-12-29 02:27:41 +0100

    create a HTTP Server and CallHandler Method

---
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -0,0 +1,9 @@
+package fr.lernejo.navy_battle;
+
+
+
+public class Launcher {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
---
+++ src/main/java/fr/lernejo/navy_battle/Serveur.java
@@ -0,0 +1,17 @@
+package fr.lernejo.navy_battle;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+public class Serveur {
+    public HttpServer launch(int port) throws IOException {
+        final HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+        httpServer.createContext("/ping", new CallHandler());
+        httpServer.setExecutor(Executors.newFixedThreadPool(1));
+
+        return httpServer;
+    }
+}
