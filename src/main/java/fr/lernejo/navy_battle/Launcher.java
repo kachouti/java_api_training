41b33e23dba6d128ded192b3ff73faa51a2e9252
kachouti <49548841+kachouti@users.noreply.github.com>
2021-12-30 22:20:00 +0100

    Update Launcher class

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -4,14 +4,12 @@ package fr.lernejo.navy_battle;
 
 public class Launcher {
     public static void main(String[] args) throws Exception {
-        if (args.length < 1) {System.out.println("Please enter a port "); System.exit(1);}
-        try{
+        
         final int port = Integer.parseInt(args[0]);
         System.out.println("listening on http://localhost:" + port + "/");
         var serv = new Serveur(port);
         if (args.length > 1)
             new Request().postRequest(port, args[1]);
-        } catch (Exception e){throw new NumberFormatException(e.getMessage());}
 
     }
 }
